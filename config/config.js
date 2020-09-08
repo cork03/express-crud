a9ca8a61843df9edacf9d677991d4bd55209e1f5
住吉翼 <sumiyoshitsubasa@tsubasanomac-book.local>
2020-09-09 08:49:51 +0900

    add options

---
+++ config/config.js
@@ -0,0 +1,21 @@
+const dorenv = require("dotenv");
+
+dorenv.config();
+
+const config = {
+  host: process.env.HOST,
+  database: process.env.DATABASE_NAME,
+  username: process.env.USER_NAME,
+  password: process.env.DATABASE_PASSWORD,
+};
+
+module.exports = {
+  development: {
+    username: config.username,
+    password: config.password,
+    database: config.database,
+    host: config.host,
+    dialect: "mysql",
+    operatorsAliases: false,
+  },
+};
